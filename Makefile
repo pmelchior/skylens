6830cac4589f7148c068a136b8f943f436d2fac3
Peter Melchior <peter.m.melchior@gmail.com>
2009-03-15 15:44:02 +0000

    simplified installation and program compilation

--- Makefile
+++ Makefile
@@ -1,19 +1,29 @@
 INCLPATH = ./include
 SRCPATH = ./src
 LIBPATH = ./lib/$(SUBDIR)
+PROGSRCPATH = ./progs
 LIBNAME = skylens
 
-SHAPELENSPATH = $(HOME)/include/shapelens
-NUMLAPATH = $(HOME)/include/numla
-ATLASPATH = $(HOME)/include/atlas
-LIBASTROPATH = $(HOME)/include/libastro
+NUMLAPATH = $(ITALIBSPATH)/include/numla
+ATLASPATH = $(ITALIBSPATH)/include/atlas
+LIBASTROPATH = $(ITALIBSPATH)/include/libastro
+SHAPELENSPATH = $(ITALIBSPATH)/include/shapelens
+SKYDBPATH = $(ITALIBSPATH)/include/skydb
+SKYLENSPATH = $(ITALIBSPATH)/include/skylens
 
 SRC = $(wildcard $(SRCPATH)/*.cc)
 OBJECTS = $(SRC:$(SRCPATH)/%.cc=$(LIBPATH)/%.o)
 HEADERS = $(wildcard $(INCLPATH)/*.h)
 
+ITALIBSLIBPATH = $(ITALIBSPATH)/lib/$(SUBDIR)
+PROGPATH = $(ITALIBSPATH)/bin/$(SUBDIR)
+PROGS = $(wildcard $(PROGSRCPATH)/*.cc)
+PROGSOBJECTS = $(PROGS:$(PROGSRCPATH)/%.cc=$(PROGPATH)/%)
+
 CC = g++
-CFLAGS = -ansi -g -Wno-deprecated -O3 -march=pentium4 -I$(INCLPATH) -I$(HOME)/include -I$(SHAPELENSPATH) -I$(NUMLAPATH) -I$(ATLASPATH) -I$(LIBASTROPATH) -DDATAPATH=$(PWD)/data
+CFLAGS = -ansi -g -Wno-deprecated -O3 -march=pentium4 -I$(INCLPATH) -I$(NUMLAPATH) -I$(ATLASPATH) -I$(LIBASTROPATH) -I$(SHAPELENSPATH) -I$(SKYDBPATH) -I$(SKYLENSPATH) -DDATAPATH=$(PWD)/data
+CFLAG_LIBS = -I$(SKYLENSPATH) -I$(HOME)/include -L$(ITALIBSLIBPATH)
+LIBS = -lskylens -lskydb -lshapelens -lastrocpp -lgsl -lcblas -llapack_atlas -latlas -llapack -lg2c -lCCfits -lcfitsio -lmysqlclient -lfftw3
 
 AR = ar
 ARFLAGS = -sr
@@ -23,16 +33,16 @@ SHAREDFLAGS = -shared -fPIC
 
 .PHONY: clean
 
-all: lib shared docs
+all: lib shared
 
 clean:
 	rm -f $(LIBPATH)/*
 
-cleanlib:
-	rm -f $(LIBPATH)/*
-
 cleanshared:
 	rm -f $(LIBPATH)/lib$(LIBNAME).so
+
+cleanprogs:
+	rm -f $(PROGSOBJECTS)
 
 lib: $(LIBPATH)/lib$(LIBNAME).a
 
@@ -40,6 +50,16 @@ shared: $(LIBPATH)/lib$(LIBNAME).so
 
 docs: $(HEADERS)
 	doxygen Doxyfile
+
+progs: $(PROGSOBJECTS)
+
+install: lib shared
+	mkdir -p $(ITALIBSLIBDIR)
+	cp $(LIBPATH)/lib$(LIBNAME).a $(ITALIBSLIBDIR)
+	cp $(LIBPATH)/lib$(LIBNAME).so $(ITALIBSLIBDIR)
+	mkdir  -p $(ITALIBSPATH)/include/$(LIBNAME)
+	cd $(INCLPATH) && find . -type f -name '*.h' -exec  cp --parents {} $(ITALIBSPATH)/include/$(LIBNAME)/ \; && cd ../
+	mkdir -p $(PROGPATH)
 
 $(LIBPATH)/lib$(LIBNAME).a: $(OBJECTS)
 	$(AR) $(ARFLAGS) $@ $?
@@ -49,3 +69,7 @@ $(LIBPATH)/lib$(LIBNAME).so: $(OBJECTS)
 
 $(LIBPATH)/%.o: $(SRCPATH)/%.cc
 	$(CC) $(CFLAGS) -c $< -o $@
+
+$(PROGPATH)/%: $(PROGSRCPATH)/%.cc
+	$(CC) $(CFLAGS) $(CFLAG_LIBS) $< -o $@ $(LIBS)
+
---
+++ development_install.py
@@ -0,0 +1,74 @@
+import os
+
+# check whether CODEDIR and ITALIBSINCLPATH are define
+CODEDIR = ""
+ITALIBSPATH = ""
+ITALIBSINCLPATH = ""
+ITALIBSLIBPATH = ""
+if os.environ.get("CODEDIR") == None:
+	print "CODEDIR variable not defined"
+	os.exit(1)
+else:
+	CODEDIR = os.environ.get("CODEDIR")
+if os.environ.get("ITALIBSPATH") == None:
+	print "ITALIBSPATH variable not defined"
+	os.exit(1)
+else:
+	ITALIBSPATH = os.environ.get("ITALIBSPATH")
+	ITALIBSINCLPATH = ITALIBSPATH + "/include"
+	ITALIBSLIBPATH = ITALIBSPATH + "/lib"
+	if os.path.exists(ITALIBSINCLPATH) == False:
+		os.system("mkdir " + ITALIBSINCLPATH)
+	if os.path.exists(ITALIBSLIBPATH) == False:
+		os.system("mkdir " + ITALIBSLIBPATH)
+	if os.path.exists(ITALIBSPATH+"/bin") == False:
+		os.system("mkdir " + ITALIBSPATH+"/bin")
+
+# check for numla files
+if os.path.isdir(CODEDIR+"/numla"):
+	if os.path.exists(ITALIBSINCLPATH + "/numla") == False:
+		os.system("ln -s " + CODEDIR+"/numla " + ITALIBSINCLPATH + "/numla");
+else:
+	print CODEDIR+"/numla does not exist."
+	os.exit(1)
+
+# check for libastro files
+if os.path.isdir(CODEDIR + "/libastro/trunk"):
+	if os.path.exists(ITALIBSINCLPATH + "/libastro") == False:
+		os.system("ln -s " + CODEDIR+"/libastro/trunk " + ITALIBSINCLPATH + "/libastro")
+	if os.path.exists(ITALIBSLIBPATH+"/libastrocpp.so") == False:
+		os.system("ln -s " + CODEDIR+"/libastro/trunk/libastrocpp.so "+ITALIBSLIBPATH+"/")
+else:
+	print CODEDIR + "/libastro/trunk does not exist."
+	os.exit(1)
+
+# check for shapelens files
+if os.path.isdir(CODEDIR + "/shapelens"):
+	if os.path.exists(ITALIBSINCLPATH + "/shapelens") == False:
+		os.system("ln -s " + CODEDIR+"/shapelens/include " + ITALIBSINCLPATH + "/shapelens")
+	if os.path.exists(ITALIBSLIBPATH+"/libshapelens.so") == False:
+		os.system("ln -s " + CODEDIR+"/shapelens/lib/libshapelens.so "+ITALIBSLIBPATH+"/")
+else:
+	print CODEDIR + "/shapelens does not exist."
+	os.exit(1)
+
+# check for skylens files
+if os.path.isdir(CODEDIR + "/skydb"):
+	if os.path.exists(ITALIBSINCLPATH + "/skydb") == False:
+		os.system("ln -s " + CODEDIR+"/skydb/include " + ITALIBSINCLPATH + "/skydb")
+	if os.path.exists(ITALIBSLIBPATH+"/libskydb.so") == False:
+		os.system("ln -s " + CODEDIR+"/skydb/lib/libskydb.so "+ITALIBSLIBPATH+"/")
+else:
+	print CODEDIR + "/skydb does not exist."
+	os.exit(1)
+
+# check for skylens files
+if os.path.isdir(CODEDIR + "/skylens"):
+	if os.path.exists(ITALIBSINCLPATH + "/skylens") == False:
+		os.system("ln -s " + CODEDIR+"/skylens/include " + ITALIBSINCLPATH + "/skylens")
+	if os.path.exists(ITALIBSLIBPATH+"/libskylens.so") == False:
+		os.system("ln -s " + CODEDIR+"/skylens/lib/libskylens.so "+ITALIBSLIBPATH+"/")
+else:
+	print CODEDIR + "/skylens does not exist."
+	os.exit(1)
+
