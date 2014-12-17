f54a0a0f134d7db8ab5c63f4dd2b5812e6ab9c9c
Peter Melchior <peter.m.melchior@gmail.com>
2014-12-17 01:10:02 -0500

    updated docs for SersicModel

--- include/SourceModel.h
+++ include/SourceModel.h
@@ -64,11 +64,12 @@ namespace skylens {
   /// The model has the form
   /// \f[I_S\bigl((x,y)\bigl) = exp\Bigl\lbrace -b_n\Bigl[\Bigl(\frac{r}{R_e}\Bigr)^{1/n} -1\Bigr]\Bigr\rbrace\f]
   /// with \f$r=\sqrt{x^2 + y^2}\f$ and \f$b_n\f$ defined by the relation between the complete and the incompete Gamma function,
-  /// \f[\Gamma(2n)=2\gamma(2n,b_n)\ \Rightarrow\ b_n\approx 1.9992 n - 0.3271.\f]
+  /// \f[\Gamma(2n)=2\gamma(2n,b_n)\ \Rightarrow\ b_n\approx 2 n - 1/3 + 0.009876/n.\f]
   /// The ensure vanishing flux at large radii, the profile is truncated at 
   /// \f$5R_e\f$ and the appropriate value at that position is subtracted from 
   /// \f$I_S\f$.\n\n
-  /// Details can be seen in Graham & Driver, 2005, PASA, 22, 118-127.
+  /// Details can be seen in Graham & Driver, 2005, PASA, 22, 118-127 and
+  /// Prugniel & Simien, 1997, A&A, 321, 111.
   class SersicModel : public SourceModel {
   public:
     /// Constructor with Sersic index \p n, effective radius \p Re, \p flux, and
